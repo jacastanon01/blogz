f918d7001471d8b4cc752d8e59048d37b72857ca
Jacob Castanon <Jacastanon01@gmail.com>
2017-12-29 18:17:54 -0600

    created index, login, and signup template. Created new database for blogz

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogger@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'whatever#'
